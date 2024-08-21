96b51bc92dc89e3e99b6357f14c8f02e780e4eab
Rhylei Tremlett <Rhylei@aureate.dev>
2024-08-21 20:24:49 +1200

    fixed mainmenu preload

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,28 +1,37 @@
 import { Scene } from 'phaser';
+import WebFont from 'webfontloader';
 
-export class MainMenu extends Scene
-{
-    constructor ()
-    {
+export class MainMenu extends Scene {
+    constructor() {
         super('MainMenu');
     }
 
-    create ()
-    {
-        this.add.image(512, 384, 'background');
+    preload() {
+        // Load the web font using WebFont Loader
+        this.load.script('webfont', 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
+    }
 
-        this.add.image(512, 380, 'logo');
+    create() {
+        // Ensure the font is loaded before rendering text
+        WebFont.load({
+            custom: {
+                families: ['Pixelify Sans'],
+                urls: ['/style.css'] // Update this path to your actual font CSS file
+            },
+            active: () => {
+                this.add.image(512, 384, 'background');
+                this.add.image(512, 380, 'logo');
 
-        this.add.text(512, 460, 'Play', {
-            fontFamily: "Pixelify Sans", fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 6,
-            align: 'center'
-        }).setOrigin(0.5);
+                this.add.text(512, 460, 'Play', {
+                    fontFamily: "Pixelify Sans", fontSize: 38, color: '#ffffff',
+                    stroke: '#000000', strokeThickness: 6,
+                    align: 'center'
+                }).setOrigin(0.5);
 
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('Mining');
-
+                this.input.once('pointerdown', () => {
+                    this.scene.start('Mining');
+                });
+            }
         });
     }
 }
